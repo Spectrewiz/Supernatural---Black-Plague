0b8ca7a4f6ad2c4fc4f0aa1262c583cf803a65b7
Spectrewiz <n3150n.email@gmail.com>
2012-02-20 19:00:35 -0500

    first commit

---
+++ EpicDiseasesDev/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TShockAPI;
+
+namespace EpicDiseases
+{
+    class Player
+    {
+        public int Index { get; set; }
+        public TSPlayer TSPlayer { get { return TShock.Players[Index]; } }
+        //Add other variables here - MAKE SURE YOU DON'T MAKE THEM STATIC
+
+        public Player(int index)
+        {
+            Index = index;
+        }
+
+        public static Player GetPlayerById(int index)
+        {
+            foreach (Player player in EpicDiseases.Players)
+            {
+                if (player != null && player.Index == index)
+                    return player;
+            }
+            return null;
+        }
+
+        // Return player by Name
+        public static Player GetPlayerByName(string name)
+        {
+            var player = TShock.Utils.FindPlayer(name)[0];
+            if (player != null)
+            {
+                foreach (Player ply in EpicDiseases.Players)
+                {
+                    if (ply.TSPlayer == player)
+                    {
+                        return ply;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ EpicDiseasesDev/PluginMain.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Drawing;
+using Terraria;
+using Hooks;
+using TShockAPI;
+using TShockAPI.DB;
+using System.ComponentModel;
+
+namespace EpicDiseases
+{
+    [APIVersion(1, 9)]
+    public class EpicDiseases : TerrariaPlugin
+    {
+        public override string Name
+        {
+            get { return "Epic Diseases DEV"; }
+        }
+
+        public override string Author
+        {
+            get { return "Spectrewiz"; }
+        }
+
+        public override string Description
+        {
+            get { return "So far theres pretty much nothing that goes on."; }
+        }
+
+        public override Version Version
+        {
+            get { return Assembly.GetExecutingAssembly().GetName().Version; }
+        }
+
+        public static List<Player> Players = new List<Player>();
+        public override void Initialize()
+        {
+            GameHooks.Update += OnUpdate;
+            GameHooks.Initialize += OnInitialize;
+            NetHooks.GreetPlayer += OnGreetPlayer;
+            ServerHooks.Leave += OnLeave;
+            ServerHooks.Chat += OnChat;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                GameHooks.Update -= OnUpdate;
+                GameHooks.Initialize -= OnInitialize;
+                NetHooks.GreetPlayer -= OnGreetPlayer;
+                ServerHooks.Leave -= OnLeave;
+                ServerHooks.Chat -= OnChat;
+            }
+            base.Dispose(disposing);
+        }
+
+        public EpicDiseases(Main game)
+            : base(game)
+        {
+        }
+
+        public void OnInitialize()
+        {
+            bool infect = false;
+
+            foreach (Group group in TShock.Groups.groups)
+            {
+                if (group.Name != "superadmin")
+                {
+                    if (group.HasPermission("say"))
+                        infect = true;
+                }
+            }
+
+            List<string> permlist = new List<string>();
+            if (!infect)
+                permlist.Add("Infect");
+            TShock.Groups.AddPermissions("trustedadmin", permlist);
+
+            Commands.ChatCommands.Add(new Command("Infect", Infect, "infect"));
+        }
+
+        public void OnUpdate()
+        {
+        }
+
+        public void OnGreetPlayer(int who, HandledEventArgs e)
+        {
+            lock (Players)
+                Players.Add(new Player(who));
+        }
+
+        public void OnLeave(int ply)
+        {
+            lock (Players)
+            {
+                for (int i = 0; i < Players.Count; i++)
+                {
+                    if (Players[i].Index == ply)
+                    {
+                        Players.RemoveAt(i);
+                        break; //Found the player, break.
+                    }
+                }
+            }
+        }
+
+        public void OnChat(messageBuffer msg, int ply, string text, HandledEventArgs e)
+        {
+        }
+
+        public static void Infect(CommandArgs args)
+        {
+            foreach (Player player in EpicDiseases.Players)
+            {
+                //Code here
+            }
+        }
+    }
+}
